35e5023284015a4f95734385debad83ddccd2e70
broothie <adbooth8@gmail.com>
2022-08-13 17:31:05 -0700

    respond handler

--- respond.go
+++ respond.go
@@ -1,6 +1,9 @@
 package ray
 
-import "net/http"
+import (
+	"bytes"
+	"net/http"
+)
 
 type Responder interface {
 	Apply(Response) Response
@@ -24,4 +27,37 @@ func (r Responders) Apply(response Response) Response {
 
 func Respond(responders ...Responder) Response {
 	return Responders(responders).Apply(Response{Status: http.StatusOK})
+}
+
+type recorder struct {
+	status int
+	header http.Header
+	body   *bytes.Buffer
+}
+
+func (r *recorder) Header() http.Header {
+	return r.header
+}
+
+func (r *recorder) Write(body []byte) (int, error) {
+	if r.body == nil {
+		r.body = new(bytes.Buffer)
+	}
+
+	return r.body.Write(body)
+}
+
+func (r *recorder) WriteHeader(statusCode int) {
+	r.status = statusCode
+}
+
+func RespondHandler(r *http.Request, handler http.Handler) Response {
+	recorder := new(recorder)
+	handler.ServeHTTP(recorder, r)
+
+	return Response{
+		Status:  recorder.status,
+		Headers: recorder.header,
+		Body:    recorder.body,
+	}
 }
